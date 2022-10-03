ddc6dda75aba7960c912d329f2c57d7f2087089e
DRAME-ibrahima <drame.ibou.07@gmail.com>
2022-10-03 22:59:02 +0200

    maj

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,36 +1,20 @@
-// //  Mouse Anim
+gsap.from(
+  ".logo, .navLink, .presentation > h1, .presentation > .what_i_do, .liens",
+  {
+    delay: 1,
+    duration: 3,
+    opacity: 0,
+    y: "100",
+    stagger: 0.09,
+    ease: Expo.easeInOut,
+  }
+);
 
-// $(document).on("mousemove", function (e) {
-//   let x = e.clientX;
-//   let y = e.clientY;
-
-//   console.log(e);
-
-//   $(".cursor").css({
-//     top: y,
-//     left: x,
-//   });
-// });
-
-// // GSAP Animations
-
-// gsap.to(".loader", {
-//   duration: 2,
-//   delay: 2,
-//   opacity: 0,
-//   ease: Expo.easeOut,
-//   display: "none",
-//   y: "100",
-// });
-
-// gsap.from(
-//   ".header > .logo, .presentation > h1, .presentation > .what_i_do, nav",
-//   {
-//     delay: 2,
-//     duration: 3,
-//     y: "200",
-//     stagger: 0.3,
-//     opacity: 0,
-//     ease: Expo.easeInOut,
-//   }
-// );
+gsap.from(".reseaux > a", {
+  delay: 2,
+  duration: 3,
+  opacity: 0,
+  x: "100",
+  stagger: 0.09,
+  ease: Expo.easeInOut,
+});
