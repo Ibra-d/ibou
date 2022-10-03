e6bfa4a6513814d4f5bbec05e86aac6c6a223a00
DRAME-ibrahima <drame.ibou.07@gmail.com>
2022-10-03 22:26:40 +0200

    maj

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,113 +1,36 @@
-$(".toggle_up").on("click", function () {
-  gsap.to(".toggle_up", {
-    x: "200",
-  });
+// //  Mouse Anim
 
-  gsap.to(".toggle_up > .bar1, .toggle_up > .bar2", {
-    position: "absolute",
-  });
+// $(document).on("mousemove", function (e) {
+//   let x = e.clientX;
+//   let y = e.clientY;
 
-  setTimeout(() => {
-    gsap.to("nav > ul", {
-      translate: "0 0",
-      ease: Expo.easeInOut,
-    });
-  }, 500);
-});
+//   console.log(e);
 
-$(".toggle_down").on("click", function () {
-  setTimeout(() => {
-    gsap.to("nav > ul", {
-      translate: "150% 0",
-      ease: Elastic.easeOut,
-    });
-  }, 500);
-
-  gsap.to(".toggle_up", {
-    delay: 1,
-    x: "0",
-  });
-
-  gsap.to(".toggle_up > .bar1, .toggle_up > .bar2", {
-    position: "relative",
-  });
-});
-
-$(document).on("mousemove", function (e) {
-  let x = e.clientX;
-  let y = e.clientY;
-
-  $(".cursor").css({
-    top: y,
-    left: x,
-  });
-
-  return { x, y };
-});
-
-$("a").mouseenter(function () {
-  $(".cursor").css({
-    height: "60px",
-    width: "60px",
-    "background-color": "transparent",
-    border: "1px solid #000",
-  });
-});
-
-$("a").mouseleave(function () {
-  $(".cursor").css({
-    height: "20px",
-    width: "20px",
-    "background-color": "#000",
-    border: "none",
-  });
-});
-
-gsap.from("nav > ul > a, header > .logo, section ", {
-  delay: 3,
-  y: "200",
-  opacity: 0,
-  stagger: 0.09,
-  duration: 4,
-  ease: Expo.easeInOut,
-});
-
-gsap.from(".medias > a", 4, {
-  delay: 5,
-  x: "200",
-  opacity: 0,
-  stagger: 0.09,
-  ease: Expo.easeInOut,
-});
-
-gsap.from(".text_subs", 1, {
-  scale: 0,
-  ease: Elastic.easeOut,
-  delay: 6,
-});
-
-gsap.to(".loader", 2, {
-  top: 0,
-  ease: Elastic.easeOut,
-});
-
-gsap.to(".loader", 2, {
-  ease: Expo.easeInOut,
-  delay: 3,
-  left: "100%",
-});
-
-// const dataImg = document.querySelectorAll(".medias > a");
-// dataImg.forEach((el) => {
-//   const attr = el.dataset.img;
-
-//   el.addEventListener("mouseenter", function (e) {
-//     const img = document.querySelector(".image_de_reseau");
-//     img.src = attr;
-//   });
-
-//   el.addEventListener("mouseleave", function (e) {
-//     const img = document.querySelector(".image_de_reseau");
-//     img.src = "";
+//   $(".cursor").css({
+//     top: y,
+//     left: x,
 //   });
 // });
+
+// // GSAP Animations
+
+// gsap.to(".loader", {
+//   duration: 2,
+//   delay: 2,
+//   opacity: 0,
+//   ease: Expo.easeOut,
+//   display: "none",
+//   y: "100",
+// });
+
+// gsap.from(
+//   ".header > .logo, .presentation > h1, .presentation > .what_i_do, nav",
+//   {
+//     delay: 2,
+//     duration: 3,
+//     y: "200",
+//     stagger: 0.3,
+//     opacity: 0,
+//     ease: Expo.easeInOut,
+//   }
+// );
